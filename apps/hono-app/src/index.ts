76178bb272dd07df0f02a371be37936c2d31c227
Carlos Ziegler <carlos.ziegler@gmail.com>
2024-01-14 14:32:31 +0100

    🔧 chore(index.ts): remove console.log(app.routes) 🔧 chore(index.ts): remove unused import statement

--- apps/hono-app/src/index.ts
+++ apps/hono-app/src/index.ts
@@ -6,7 +6,7 @@ const app = new Hono();
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
-console.log(app.routes);
+
 const port = 3000;
 console.log(`Server is running on port ${port}`);
 
